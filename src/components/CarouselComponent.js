971cb744967600fbdd58f5b07675a796c415647c
John Gilday <jgilday01@gmail.com>
2020-09-26 12:48:03 -0400

    Carousel addon

--- src/components/CarouselComponent.js
+++ src/components/CarouselComponent.js
@@ -11,17 +11,26 @@ const items = [
     {
         src: 'assets/images/muffin.jpg',
         altText: 'Muffin photo by david-todd-mccarty',
+        captionHead: 'Tasty Tuesday',
         caption: 'Reduced Muffin Prices'
     },
     {
         src: 'assets/images/acoustic.jpg',
         altText: 'Acoustic guitar by rachmaddian-shotz',
+        captionHead: 'Wednesday Nights',
         caption: 'LIVE Acoustic Music'
     },
     {
         src: 'assets/images/mule-top.jpg',
         altText: 'Mule top by alex-plesovskich',
+        captionHead: 'Thirsty Thursday',
         caption: 'Reduced Mule Prices'
+    },
+    {
+        src: 'assets/images/dice.jpg',
+        altText: 'Dice by brett-jordan',
+        captionHead: 'Funtime Friday',
+        caption: '*** Game Night ***'
     }
 ];
 
@@ -54,7 +63,7 @@ const Example = (props) => {
                 key={item.src}
             >
                 <img className="d-block w-100" src={item.src} alt={item.altText} />
-                <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+                <CarouselCaption captionText={item.caption} captionHeader={item.captionHead} />
             </CarouselItem>
         );
     });
