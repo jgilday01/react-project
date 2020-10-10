8c41eb299ed28a5e203c0b7b6bb2c7e9d525cb30
John Gilday <jgilday01@gmail.com>
2020-10-10 16:23:53 -0400

    reset after test

--- src/shared/banners.js
+++ src/shared/banners.js
@@ -1,19 +1,19 @@
 export const BANNERS = [
     {
         id: 1,
-        src: '/assets/images/muffins.jpg',
+        src: 'assets/images/muffins.jpg',
         alt: 'muffins by roberto-martinez',
         txt: 'Muffins'
     },
     {
         id: 2,
-        src: './assets/images/meatballs.jpg',
+        src: 'assets/images/meatballs.jpg',
         alt: 'meatball by jason-leung"',
         txt: 'Meatballs'
     },
     {
         id: 3,
-        src: '/assets/images/mules.jpg',
+        src: 'assets/images/mules.jpg',
         alt: 'mule drink by miguel-maldonado',
         txt: 'Mules'
     }
